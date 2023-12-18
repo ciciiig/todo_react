f9a6292078286f93b978126edd6dab92b6d71154
George <tsintsadze7@gmail.com>
2023-12-18 12:37:25 +0400

    fix: duplicated App route inside App

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export const App: FC = () => {
     <Router>
       <main className='app'>
         <Routes>
-          <Route path="/" element={<App />} />
+          {/* <Route path="/" element={<App />} /> */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
