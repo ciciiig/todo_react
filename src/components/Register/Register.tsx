d838ab63eb9705b1adb01ef1c3528aaff67937d1
George <tsintsadze7@gmail.com>
2023-12-18 06:32:39 +0400

    feat: add placeholder to input field

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,5 @@
 import "./Register.css"
-import { FC } from "react"
+import { FC, FormEvent } from "react"
 import { useRef, useState, useEffect } from "react"
 
 const MAIL_REGEX = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
@@ -12,6 +12,8 @@ export const Register: FC = () => {
     const [user, setUser] = useState('')
     const [validName, setValidName] = useState(false)
     const [userFocus, setUserFocus] = useState(false)
+    
+    const [usersList, setUsersList] = useState<string[]>([])    
 
     const [password, setPassword] = useState('')
     const [validPassword, setValidPassword] = useState(false)
@@ -21,76 +23,106 @@ export const Register: FC = () => {
     const [success, setSuccess] = useState(false)
 
     useEffect(() => {
+        const storedUsers = localStorage.getItem('username');
+        if (storedUsers) {
+            setUsersList(JSON.parse(storedUsers));
+        }
+
         if (userRef.current) {
             userRef.current.focus();
         }
     }, [])
 
     useEffect(() => {
-        const result = MAIL_REGEX.test(user)
-        console.log(result)
-        console.log(user)
-        setValidName(result)
+        setValidName(MAIL_REGEX.test(user))
     }, [user])
 
     useEffect(() => {
-        const result = PASSWORD_REGEX.test(password)
-        console.log(result)
-        console.log(password)
-        setValidPassword(result)
+        setValidPassword(PASSWORD_REGEX.test(password))
     }, [password])
 
     useEffect(() => {
         setErrMsg('')
-    }, [user, password])
+    }, [user, usersList, password])
+
+    useEffect(() => {
+        localStorage.setItem('username', JSON.stringify(usersList))
+    }, [usersList])
+
+    const checkIfUsernameExists = (usernameEmail: string) => {
+        console.log(usersList);
+        
+        if (!usersList.includes(usernameEmail)) {
+            setUsersList(prevUsers => [...prevUsers, usernameEmail])
+            setSuccess(true)
+        } else {
+            setErrMsg("User already exists")
+            setSuccess(false)
+        }
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        checkIfUsernameExists(user)
+    }
 
     return (
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "hide"}>{errMsg}</p>
-            <h1>Register</h1>
-            <form>
-                <label htmlFor="username">
-                    Username:
-                    <span className={validName ? "valid" : "hide"}>✔
-                    </span>
-                    <span className={validName || !user ? "hide" : "invalid"}>✘
-                    </span>
-                </label>
-                <input
-                    type="text"
-                    id="username"
-                    ref={userRef}
-                    autoComplete="off"
-                    onChange={(e) => setUser(e.target.value)}
-                    required
-                    onFocus={() => setUserFocus(true)}
-                    onBlur={() => setUserFocus(false)}
-                />
-                <p className={userFocus && user && !validName ? "instructions" : "hide"}>
-                    Valid Email only
-                </p>
+        <>
+            {success ? (
+                <section>
+                    <h1>Registration completed!</h1>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "hide"}>{errMsg}</p>
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">
+                            Username:
+                            <span className={validName ? "valid" : "hide"}>✔
+                            </span>
+                            <span className={validName || !user ? "hide" : "invalid"}>✘
+                            </span>
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            placeholder="someone@example.com"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            required
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p className={userFocus && user && !validName ? "instructions" : "hide"}>
+                            Valid Email only
+                        </p>
 
-                <label htmlFor="password">
-                    Password:
-                    <span className={validPassword ? "valid" : "hide"}>✔
-                    </span>
-                    <span className={validPassword || !password ? "hide" : "invalid"}>✘
-                    </span>
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    onFocus={() => setPasswordFocus(true)}
-                    onBlur={() => setPasswordFocus(false)}
-                />
-                <p className={passwordFocus && !validPassword ? "instructions" : "hide"}>
-                    - at least 8 characters<br />
-                    - must contain at least 1 uppercase, lowercase and number<br />
-                    - Can contain special characters
-                </p>
-            </form>
-        </section>
+                        <label htmlFor="password">
+                            Password:
+                            <span className={validPassword ? "valid" : "hide"}>✔
+                            </span>
+                            <span className={validPassword || !password ? "hide" : "invalid"}>✘
+                            </span>
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            onFocus={() => setPasswordFocus(true)}
+                            onBlur={() => setPasswordFocus(false)}
+                        />
+                        <p className={passwordFocus && !validPassword ? "instructions" : "hide"}>
+                            - at least 8 characters<br />
+                            - must contain at least 1 uppercase, lowercase and number<br />
+                            - Can contain special characters
+                        </p>
+                        <button disabled={!validName || !validPassword}>Sign Up</button>
+                    </form>
+                </section>
+            )}
+        </>
     )
 }
